66716cdd175b679de3717d436cc826952ad37fa3
javiersanchzgil <javsan675@alu.centroafuera.es>
2023-01-19 13:28:14 +0100

    Fin

--- app/src/main/java/com/example/ejer6repasopmdm/MainActivity.kt
+++ app/src/main/java/com/example/ejer6repasopmdm/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import androidx.core.view.isInvisible
 import com.example.ejer6repasopmdm.databinding.ActivityMainBinding
 
@@ -16,89 +17,63 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.boton.text = "COMENZAR"
-        binding.nombre.isInvisible = true
+        texto(binding)
 
-        binding.boton.setOnClickListener {
-            binding.nombre.isInvisible = false
-            binding.boton.text = "FINALIZAR"
-        }
 
-        binding.nombre.setOnClickListener {
-            if (binding.nombre.text.isEmpty()) {
-                binding.boton.isEnabled = true
-            } else {
-                if (binding.nombre.text.isNotEmpty()) {
-                    binding.boton.isEnabled = false
-                    if (binding.nombre.text.toString() == "Wayne"){
+            binding.nombre.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
 
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (binding.nombre.text.toString().isNotEmpty()) {
+                        binding.boton.isEnabled = true
+                        if (binding.nombre.text.contains("Wayne", ignoreCase = true)) {
+                            binding.imagen.isInvisible = false
+                        } else
+                            if (binding.nombre.text.contains("Joker", ignoreCase = true)) {
+                                binding.root.setBackgroundColor(resources.getColor(R.color.purple_700))
+                                binding.boton.setBackgroundColor(resources.getColor(R.color.black))
+                                binding.nombre.setTextColor(resources.getColor(R.color.white))
+                                binding.imagen.isInvisible = true
+                            } else{
+                                    binding.imagen.isInvisible = true
+                                    binding.root.setBackgroundColor(resources.getColor(R.color.white))
+                                    binding.boton.setBackgroundColor(resources.getColor(R.color.purple_500))
+                                    binding.nombre.setTextColor(resources.getColor(R.color.black))
+
+                            }
 
                     }
+
                 }
-        }
-        }
 
-        binding.boton.setOnClickListener {
-            binding.nombre.text.clear()
-            binding.boton.text = "COMENZAR"
-        }
+                override fun afterTextChanged(s: Editable?) {
 
-
-
-
-
-
-
-        /*binding.boton.setOnClickListener {
-            binding.boton.text = "SALUDAR"
-            binding.editText.visibility = binding.editText.visibility
-            binding.editText.hint = "Introduce tu nombre"
-            binding.editText.setText("")
-            binding.editText.requestFocus()
-            binding.boton.setOnClickListener {
-                binding.boton.text = "VOLVER A EMPEZAR"
-                binding.editText.visibility = binding.editText.visibility
-                binding.editText.hint = "Hola ${binding.editText.text}"
-                binding.editText.setText("")
-                binding.editText.requestFocus()
-                binding.boton.setOnClickListener {
-                    binding.boton.text = "COMENZAR"
-                    binding.editText.visibility = binding.editText.visibility
-                    binding.editText.hint = "Introduce tu nombre"
-                    binding.editText.setText("")
-                    binding.editText.requestFocus()
                 }
+            })
+
+
+        }
+    fun texto(binding: ActivityMainBinding) {
+        binding.boton.setOnClickListener() {
+            if (binding.nombre.isInvisible) {
+                binding.nombre.visibility = View.VISIBLE
+                binding.boton.text = "Finalizar"
+            } else {
+                binding.nombre.visibility = View.INVISIBLE
+                binding.boton.text = "Comenzar"
             }
-        }*/
-
-
-
-        binding.nombre.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
-
-
-
-
-
-
-
-
-
+        }
 
 
     }
 }
+
 
 
